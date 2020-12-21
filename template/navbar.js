f5c112790f99eea3afd4e0e3bd7a25521848e515
MiguelARam27 <garandster27@live.com>
2020-12-21 01:30:25 -0500

    Landing page portion done

---
+++ template/navbar.js
@@ -0,0 +1,9 @@
+let button = document.getElementsByClassName('navbar__togglebutton')[0];
+let navBarLinks = document.getElementsByClassName('navbar__links')[0];
+
+button.addEventListener('click', () => {
+  console.log('hello');
+  navBarLinks.classList.toggle('active');
+});
+
+const example = { a: 'lol', b: 'b', c: 'top', exfjasdlfjaldfjkla: 'top' };
