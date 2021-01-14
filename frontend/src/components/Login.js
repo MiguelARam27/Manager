6fcb8b7140950c33d1488772e50c6db9005cc242
MiguelARam27 <garandster27@live.com>
2021-01-13 22:20:57 -0500

    changed loading on login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -4,6 +4,7 @@ import { login } from '../actions/userActions';
 import { useSelector } from 'react-redux';
 import Message from './Message';
 import { useHistory } from 'react-router-dom';
+import Loading from './Loading';
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -13,7 +14,7 @@ const Login = () => {
   const [show, setShow] = useState(false);
 
   const userLogin = useSelector((state) => state.userLogin);
-  const { error, success, userInfo } = userLogin;
+  const { error, success, userInfo, loading } = userLogin;
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -43,31 +44,37 @@ const Login = () => {
   return (
     <form className='signup' onSubmit={submitHandler}>
       <h1 className='heading-title'>Login</h1>
-      <div className='signup__input-container'>
-        <input
-          type='email'
-          required
-          placeholder={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-      </div>
-      <div className='signup__input-container'>
-        <input
-          type='password'
-          required
-          placeholder={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-      </div>
-      <button type='submit' onClick={submitHandler}>
-        Submit
-      </button>
-      {error && show && <Message variant={'danger'}>{error}</Message>}
-      {success && show && <Message variant={'success'}>Welcome!</Message>}
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <div className='signup__input-container'>
+            <input
+              type='email'
+              required
+              placeholder={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+          <div className='signup__input-container'>
+            <input
+              type='password'
+              required
+              placeholder={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <button type='submit' onClick={submitHandler}>
+            Submit
+          </button>
+          {error && show && <Message variant={'danger'}>{error}</Message>}
+          {success && show && <Message variant={'success'}>Welcome!</Message>}
+        </>
+      )}
     </form>
   );
 };
